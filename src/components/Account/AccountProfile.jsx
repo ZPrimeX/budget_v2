443587625aafb3486e0ce2b2396b68dc932f0a7c
Shah <shahmansurov1001@gmail.com>
2022-10-18 16:37:37 -0400

    changed schema prisma

--- src/components/Account/AccountProfile.jsx
+++ src/components/Account/AccountProfile.jsx
@@ -25,11 +25,11 @@ const AccountProfile = (props) => {
               }}
             />
             <Box display={"flex"} gap={1}>
-              <Typography color="textPrimary" gutterBottom variant="h5">
-                {user.first_name.charAt(0).toUpperCase() + user.first_name.slice(1)}
+              <Typography color="textPrimary" gutterBottom variant="h5" textTransform={"capitalize"}>
+                {user.first_name}
               </Typography>
-              <Typography color="textPrimary" gutterBottom variant="h5">
-                {user.last_name.charAt(0).toUpperCase() + user.last_name.slice(1)}
+              <Typography color="textPrimary" gutterBottom variant="h5" textTransform={"capitalize"}>
+                {user.last_name}
               </Typography>
             </Box>
             <Typography color="textSecondary" variant="body2">
