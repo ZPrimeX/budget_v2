d17a4f09c8b006c299aacf14ed0445e415970898
Shah <shahmansurov1001@gmail.com>
2022-11-27 14:25:54 -0500

    fixed transaction api

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,29 +1,5 @@
 import prisma from "../lib/prisma";
 
-export const findAll = async (wallet) => {
-  const data = await prisma.transaction.findMany({
-    where: {
-      wallet_id: wallet.id,
-    },
-    include: {
-      category: true,
-    },
-  });
-  return data;
-};
-
-export const findUnique = async (transaction) => {
-  const transactions = await prisma.transaction.findUnique({
-    where: {
-      id: transaction.id,
-    },
-    include: {
-      category: true,
-    },
-  });
-  return transactions;
-};
-
 export const create = async (data) => {
   try {
     const newTransaction = await prisma.transaction.create({
@@ -53,46 +29,17 @@ export const create = async (data) => {
   }
 };
 
-export const edit = async (data) => {
-  try {
-    const editTransaction = await prisma.transaction.update({ where: { id: data.id }, data: data.body });
-
-    let payload;
-    if (category.category_type === "expense") {
-      payload = { balance: { decrement: data.amount }, expense: { increment: data.amount } };
-    } else {
-      payload = { balance: { increment: data.amount }, income: { increment: data.amount } };
-    }
-
-    await prisma.wallet.update({
-      where: { id: data.wallet_id },
-      data: payload,
-    });
-
-    return editTransaction;
-  } catch (error) {
-    throw new Error(error);
+export const updateBalance = async (category_id, data) => {
+  const category = await prisma.category.findUnique({ where: { id: category_id } });
+  let payload;
+  if (category.category_type === "expense") {
+    payload = { balance: { decrement: data.amount }, expense: { increment: data.amount } };
+  } else {
+    payload = { balance: { increment: data.amount }, income: { increment: data.amount } };
   }
-};
 
-export const deleteTransaction = async (data) => {
-  try {
-    const deletedTransaction = await prisma.transaction.delete({ where: { id: data.id } });
-
-    let payload;
-    if (category.category_type === "expense") {
-      payload = { balance: { decrement: data.amount }, expense: { increment: data.amount } };
-    } else {
-      payload = { balance: { increment: data.amount }, income: { increment: data.amount } };
-    }
-
-    await prisma.wallet.update({
-      where: { id: data.wallet_id },
-      data: payload,
-    });
-
-    return deletedTransaction.id;
-  } catch (error) {
-    throw new Error(error);
-  }
+  await prisma.wallet.update({
+    where: { id: data.wallet_id },
+    data: payload,
+  });
 };
--- src/pages/api/transaction/[wallet_id].js
+++ src/pages/api/transaction/[wallet_id].js
@@ -1,3 +1,4 @@
+import { updateBalance } from "../../../../server/controllers/transactionController";
 import { apiHandler } from "../../../../server/helpers/api-handler";
 import { ServerError, Success } from "../../../../server/helpers/requestValidators";
 import prisma from "../../../../server/lib/prisma";
@@ -23,26 +24,21 @@ async function handler(req, res) {
       }
     case "PATCH":
       try {
-        const { id } = req.query;
-        const transaction = await prisma.transaction.patch({
-          where: {
-            id: id,
-          },
-          data: req.body,
-        });
-        return Success(res, transaction);
+        const editTransaction = await prisma.transaction.update({ where: { id: data.id }, data: data.body });
+
+        await updateBalance(editTransaction.category_id, data);
+
+        return Success(res, editTransaction);
       } catch (error) {
         ServerError(res, error);
       }
     case "DELETE":
       try {
-        const { id } = req.query;
-        await prisma.transaction.delete({
-          where: {
-            id: id,
-          },
-        });
-        return Success(res, id);
+        const deletedTransaction = await prisma.transaction.delete({ where: { id: data.id } });
+
+        await updateBalance(deletedTransaction.category_id, data);
+
+        return Success(res, deletedTransaction.id);
       } catch (error) {
         return res.status(400).json({ message: "Transaction Not Found!" });
       }
