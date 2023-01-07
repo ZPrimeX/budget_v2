2e88843080288f4c8b787343c8c660f5180235d5
Shah <shahmansurov1001@gmail.com>
2023-01-07 12:38:21 -0500

    welcome email

--- src/pages/api/user/sign-up.js
+++ src/pages/api/user/sign-up.js
@@ -8,6 +8,19 @@ import {
   NoPassword,
   PasswordLength,
 } from "../../../../server/helpers/requestValidators";
+import sgMail from "@sendgrid/mail";
+import { welcome } from "../../../../server/sengrid_template/welcome_template";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = (userEmail, first_name, token) => {
+  return {
+    to: userEmail, // Change to your recipient
+    from: "shahmansurov1001@gmail.com", // Change to your verified sender
+    subject: `Welcome ${first_name}!`,
+    text: "You have successfully signed up.",
+    html: welcome(first_name),
+  };
+};
 
 export default apiHandler(handler);
 
@@ -32,13 +45,13 @@ async function handler(req, res) {
     res.status(401).json({ message: result });
   }
   res.status(201).json({ message: "success", body: result });
-  // const message = msg(req.body.email, req.body.username, result.token);
-  // sgMail
-  //   .send(message)
-  //   .then(() => {
-  //     console.log("Email sent");
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
+  const message = msg(req.body.email, req.body.first_name, result.token);
+  sgMail
+    .send(message)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
